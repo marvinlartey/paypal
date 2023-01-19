80f2a8bcf1724d594e17a1d8077c00f63fbbf1fc
marvinlartey <75137498+marvinlartey@users.noreply.github.com>
2023-01-19 10:13:13 +0000

    created new files for pages

---
+++ lib/pages/contacrs.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Contacts extends StatelessWidget {
+  const Contacts({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        child: Text("Contacts"),
+      ),
+    );
+  }
+}
